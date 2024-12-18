1e1214a102962e8bb754afccf4f971cb8b965eff
mj <mu8786@gmail.com>
2024-12-18 21:19:39 +0900

    ghcr 에 패키지 푸시 git action 추가

---
+++ db-run.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+docker run -d \
+--name polar-postgres \
+--net catalog-network \
+-e POSTGRES_USER=user \
+-e POSTGRES_PASSWORD=password \
+-e POSTGRES_DB=polardb_catalog \
+-p 5432:5432 \
+postgres:14.5
